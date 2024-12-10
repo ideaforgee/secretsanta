29ab620e219dfaba6d86c3e9d905ba9f269728cc
IdeaaForgee <ideaaforgee@gmail.com>
2024-12-11 00:22:19 +0530

    Updated deployment file

--- deployment/jenkins-deployment.groovy
+++ deployment/jenkins-deployment.groovy
@@ -3,14 +3,15 @@ pipeline {
     
     parameters {
         string(name: 'BRANCH_NAME', defaultValue: 'main', description: 'Branch to deploy')
-        string(name: 'BACKEND_BUILD_NUMBER', description: 'Build number for Backend')
-        string(name: 'UI_BUILD_NUMBER', description: 'Build number for UI')
+        string(name: 'BACKEND_BUILD_NUMBER', defaultValue: '241210-1817', description: 'Build number for Backend')
+        string(name: 'UI_BUILD_NUMBER', defaultValue: '241210-1817', description: 'Build number for UI')
     }
 
     environment {
         SSH_USER = 'root'
         SSH_HOST = '192.168.20.41'
         SSH_KEY = 'SSH_PASS_CREDENTIALS_BB-1-LXC-2'
+        GIT_CREDENTIALS_ID = 'Secret-Santa-Jenkins'
     }
 
     stages {
@@ -20,6 +21,17 @@ pipeline {
                 echo "UI Build Number: ${params.UI_BUILD_NUMBER}"
             }
             
+        }
+
+        stage('Docker Login') {
+            steps {
+                script {
+                    echo "Logging into Docker registry using Jenkins credentials..."
+                    docker.withRegistry('https://ghcr.io', GIT_CREDENTIALS_ID) {
+                        echo "Successfully logged in to Docker registry"
+                    }
+                }
+            }
         }
         
         stage('SSH to Server and Run Commands') {
