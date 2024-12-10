5a13bf8bbd4b8b827cf289c9e031efb1a8e09a98
IdeaaForgee <ideaaforgee@gmail.com>
2024-12-10 22:14:29 +0530

    Added secret-santa docker config.

---
+++ deployment/jenkins-build.groovy
@@ -0,0 +1,103 @@
+pipeline {
+    agent any
+
+    environment {
+        GIT_CREDENTIALS_ID = 'secret-santa-jenkins'
+        BUILD_TAG = 'latest'
+        UI_IMAGE = 'secret-santa-ui'
+        BACKEND_IMAGE = 'secret-santa-backend'
+    }
+
+    parameters {
+        string(name: 'BRANCH_NAME', defaultValue: 'main', description: 'Branch to build')
+    }
+
+    stages {
+        stage('Build Name') {
+            steps {
+                script {
+                    // Generate a timestamp for the tag
+                    BUILD_TAG = new Date().format("yyMMdd-HHmm")
+                    currentBuild.displayName = BUILD_TAG
+                }
+            }
+        }
+
+        stage('Checkout') {
+            steps {
+                script {
+                    echo "Checking out branch: ${params.BRANCH_NAME}"
+                    git branch: "${params.BRANCH_NAME}", credentialsId: "${GIT_CREDENTIALS_ID}", url: 'https://github.com/IdeaaForgee/secretSanta.git'
+                }
+            }
+        }
+
+        stage('Build Docker Images') {
+            steps {
+                script {
+                    // Build Docker images
+                    dir("${WORKSPACE}") {
+                        echo "----------------- Comencing build process -----------------"
+                        sh 'docker compose build'
+                    }
+                }
+            }
+        }
+
+         stage('Tag Docker Images') {
+            steps {
+                script {
+                    // Tag Docker images 
+                    dir("${WORKSPACE}") {
+
+                        sh "docker tag secret-santa-ui secret-santa-ui:${BUILD_TAG}"
+                        sh "docker tag secret-santa-backend secret-santa-backend:${BUILD_TAG}"
+
+                        sh "docker image tag secret-santa-ui:${BUILD_TAG} github.com/IdeaaForgee/secret-santa/secret-santa-ui:${BUILD_TAG}"
+                        sh "docker image tag secret-santa-backend:${BUILD_TAG} github.com/IdeaaForgee/secret-santa/secret-santa-backend:${BUILD_TAG}"
+
+                        echo "----------------- Tagged Docker images Build:${BUILD_TAG}  -----------------"
+                    }
+
+                }
+            }
+        }
+
+        stage('Push Docker Images to registry') {
+            steps {
+                script {
+                    // Push Docker images 
+                    dir("${WORKSPACE}") {
+                        
+                        echo "----------------- Login to registry -----------------"
+                        
+                        docker.withRegistry('https://github.com', GIT_CREDENTIALS_ID) {
+                            
+                            def uiImage = docker.image("github.com/IdeaaForgee/secret-santa/secret-santa-ui:${BUILD_TAG}")
+                            echo "----------------- Pushing UI image to registry -----------------"
+                            uiImage.push()
+                        
+                        }
+
+                        docker.withRegistry('https://github.com', GIT_CREDENTIALS_ID) {
+
+                            def backendImage = docker.image("github.com/IdeaaForgee/secret-santa/secret-santa-backend:${BUILD_TAG}")
+                            echo "----------------- Pushing Backend image to registry -----------------"
+                            backendImage.push()
+                        
+                        }
+                    }
+
+                    echo "----------------- Finished -----------------"
+                    echo "----------------- Build Number : ${BUILD_TAG} -----------------"
+                }
+            }
+        }
+    }
+
+    post {
+        always {
+            echo "Pipeline completed"
+        }
+    }
+}
---
+++ deployment/jenkins-deployment.groovy
@@ -0,0 +1,52 @@
+pipeline {
+    agent any
+    
+    parameters {
+        string(name: 'BRANCH_NAME', defaultValue: 'main', description: 'Branch to deploy')
+        string(name: 'BACKEND_BUILD_NUMBER', description: 'Build number for Backend')
+        string(name: 'UI_BUILD_NUMBER', description: 'Build number for UI')
+    }
+
+    environment {
+        SSH_USER = 'root'
+        SSH_HOST = '192.168.20.41'
+        SSH_KEY = 'SSH_PASS_CREDENTIALS_BB-1-LXC-2'
+    }
+
+    stages {
+        stage('Print Build Numbers and set Environment') {
+            steps {
+                echo "Backend Build Number: ${params.BACKEND_BUILD_NUMBER}"
+                echo "UI Build Number: ${params.UI_BUILD_NUMBER}"
+            }
+            
+        }
+        
+        stage('SSH to Server and Run Commands') {
+            steps {
+                
+                sshagent(credentials: [SSH_KEY]) {
+                  sh """
+                      [ -d ~/.ssh ] || mkdir ~/.ssh && chmod 0700 ~/.ssh
+                      ssh-keyscan -t rsa,dsa ${SSH_HOST} >> ~/.ssh/known_hosts
+                      
+                      ssh -t -t  ${SSH_USER}@${SSH_HOST} 'bash -s << 'ENDSSH'
+                              cd secret-santa
+                              UI_BUILD_NUMBER=${params.UI_BUILD_NUMBER} BACKEND_BUILD_NUMBER=${params.BACKEND_BUILD_NUMBER} docker compose up -d
+                              docker compose logs
+                              docker compose logs | grep -q "Liquibase command 'update' was executed successfully."
+                              if [ \$? -ne 0 ]; then
+                                  error "Liquibase migration failed. Exiting job."
+                                  exit 1
+                              fi
+                              exit
+                        ENDSSH'
+                      """
+                }
+                
+                
+            }
+            
+        }
+    }
+}
