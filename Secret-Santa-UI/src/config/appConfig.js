43b0d9846344b542cf18ce4151775e768fa777ec
IdeaaForgee <ideaaforgee@gmail.com>
2024-12-11 12:49:08 +0530

    Updated backend url

--- Secret-Santa-UI/src/config/appConfig.js
+++ Secret-Santa-UI/src/config/appConfig.js
@@ -1,6 +1,6 @@
 const APP_CONFIG = {
-    BACKEND_URL: 'http://localhost:5002',
-    WS_BASE_URL: 'ws://localhost:5002',
+    BACKEND_URL: 'https://secret-santa.thecodeinsight.com',
+    WS_BASE_URL: 'ws://secret-santa.thecodeinsight.com',
 };
 
 export default APP_CONFIG;
