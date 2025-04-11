8ba0e6b4f3076eb1bed316bb481d3c9bdbc342d1
IdeaaForgee <ideaaforgee@gmail.com>
2025-04-12 02:45:35 +0530

    Update backend port

--- Secret-Santa-UI/src/config/appConfig.js
+++ Secret-Santa-UI/src/config/appConfig.js
@@ -1,6 +1,6 @@
 const APP_CONFIG = {
-    BACKEND_URL: 'https://secret-santa.thecodeinsight.com',
-    WS_BASE_URL: 'wss://secret-santa.thecodeinsight.com/api/notification',
+    BACKEND_URL: 'https://funzone-uat.thecodeinsight.com',
+    WS_BASE_URL: 'wss://funzone-uat.thecodeinsight.com/api/notification',
 };
 
 export default APP_CONFIG;
